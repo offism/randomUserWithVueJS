582d3e988911c0540ba62ecf2669654d08b71e48
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-08-05 13:05:55 +0300

    finished

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const app = Vue.createApp({
      async getUser(){
         const res = await fetch('https://randomuser.me/api')
         const {results} = await res.json()
-        // firstName
+        this.firstName = results[0].name.first
+        this.lastName = results[0].name.second
+        this.gender = results[0].gender
+        this.email = results[0].email
+        this.picture = results[0].picture.large
       }
     }
 })
