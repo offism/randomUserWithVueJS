9bd16f1cf23516d6cbdc109af1837012a9f07ec5
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-08-05 13:01:33 +0300

    get data from api

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = Vue.createApp({
     		gender:"male",
     		picture:"https://randomuser.me/api/portraits/men/10.jpg" 
     	}
+    },
+    methods: {
+     async getUser(){
+        const res = await fetch('https://randomuser.me/api')
+        const {results} = await res.json()
+        // firstName
+      }
     }
 })
 app.mount('#app')
